1f17f99448390a893ce9b48a952da253b59f456e
agribov <alexander_gribov@student.uml.edu>
2017-04-21 22:45:00 -0400

    Preprocessor now saves numbered files

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import numpy as np
 import tensorflow as tf
 import math
 import re
+import preprocess
 
 # Declare global variables.
 
@@ -20,38 +21,67 @@ trainingDirectory = "../cnn/questions/training"
 validationDirectory = "../cnn/questions/validation"
 smallDirectory = "../cnn/questions/subset"
 
+dict, articleList = preprocess.getTextFromFolder(smallDirectory)
+#print(len(dict))
+
 # LSTM parameters
-batchSize = 1 # this is basically hidden layer number of features
-LSTM_num_units = 10000
+batchSize = 3 # this is basically hidden layer number of features
+LSTM_num_units = 256
+n_steps = 20
+n_classes = 814
+
+# tf Graph input
+# X will be 3D tensor, 1st is articles, then words, then vectorization of word.
+x = tf.placeholder("float", [None, n_steps, n_input])
+# Y will be a single word from the vocab.
+y = tf.placeholder("float", [None, n_classes])
+
+# Define weights
+weights = {
+    'out': tf.Variable(tf.random_normal([n_hidden, n_classes]))
+}
+biases = {
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
 
 # Training method
-def trainParser(trainingDir):
+def trainParser(trainingDir, weights):
 
-    dict, articleList = getTextFromFolder(trainingDir)
-    print(dict)
+
+    # Unpack input tensor:
+    x = tf.unstack(x, n_steps, 1)
     
     # Set up LSTM Network
-    lstm = tf.contrib.rnn.BasicLSTMCell(LSTM_num_units)
+    lstm_cell = tf.contrib.rnn.BasicLSTMCell(LSTM_num_units)
     #state = tf.zeros([batchSize, lstm.state_size])
+    """
     state = lstm.zero_state(batchSize, tf.float32)
     probabilities = []
     loss = 0.0
+    """
+    outputs, state = tf.contrib.rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
 
-    outputs = []
-    
-    for article in articleList:
+    # Linear activation, using rnn inner loop last output
+    return tf.matmul(outputs[-1], weights['out']) + biases['out']    
+
+
+    #for article in articleList:
+    """
         articleVec = []
         for word in article:
             articleVec.append(dict[word])
 
-            npVec = np.asarray(articleVec)
+        npVec = np.asarray(articleVec)
+        inTensor = tf.convert_to_tensor(npVec)
+        
+        
         #print(articleVec)
-        output, state = lstm(npVec, state)
+        output, state = lstm(x, state)
         #outputs, state = tf.contrib.rnn.static_rnn(lstm, articleVec, dtype=tf.float32)
         outputs.append(output)
 
         #print(dict)
-    """
+    
     output = tf.reshape(tf.concat(axis=1, values=outputs), [-1, size])
     softmax_w = tf.get_variable(
         "softmax_w", [size, vocab_size], dtype=data_type())
@@ -65,68 +95,46 @@ def trainParser(trainingDir):
     self._final_state = state
     """
 
-        
-        
-# This function removes unnecessary words from the file. Current removed are:
-#    - URL (Word starting with http)
-#    - NULL words
-#    - Entities
+pred = trainParser(x, weights, biases)
 
-def getTextFromFolder(directory):
-    # Declare dictionary for holding words
-    # Move declaration into for loop if it
-    # should reset with every new file.
-    dict = {}
-    
-    # Declare word counter (keep with dict)
-    nWords = 0;
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
-    # Declare a list of articles. Each member (article) will be a list of words.
-    articles = []
+# Evaluate model CHANGE THIS LATER
+correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
+# Initializing the variables
+init = tf.global_variables_initializer()
 
-    for filename in os.listdir(directory):
-        words = []
-        if filename.endswith(".question"):
-            print "Processing file: "
-            print filename
-            with open(os.path.join(directory, filename), 'r') as f:
-                for line in f:
-                    for word in re.split(';|,|\*|\n| ', line):
-                        #print(word)
-                        words.append(word)
-                filterOutCrap(words)
-                #print(words)
+dict, articleList = preprocess.getTextFromFolder(trainingDir)
 
-                # Add words to dictionary
-                for word in words:
-                    if not dict.has_key(word) and isinstance(word, basestring):
-                        dict.setdefault(word, nWords);
-                        nWords += 1
-                #print dict   
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+    step = 1
+    # Keep training until reach max iterations
+    while step * batch_size < training_iters:
+        batch_x, batch_y = mnist.train.next_batch(batch_size)
+        # Reshape data to get 28 seq of 28 elements
+        batch_x = batch_x.reshape((batch_size, n_steps, n_input))
+        # Run optimization op (backprop)
+        sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+        if step % display_step == 0:
+            # Calculate batch accuracy
+            acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
+            # Calculate batch loss
+            loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
+            print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
+                  "{:.6f}".format(loss) + ", Training Accuracy= " + \
+                  "{:.5f}".format(acc))
+        step += 1
+    print("Optimization Finished!")
 
-                # Add this article to the list of articles.
-                articles.append(words)
-                
-            # Comment out break if you want to run on more than one file
-            #break
-    
-    return dict, articles
-
-
-def filterOutCrap(text):
-    for word in text:
-        if not (word):
-            #This should just remove NULL words -- why does uncommenting that stop entities from being removed? Because of the indexing?
-            #text.remove(word)
-            continue
-        elif word[0] == '@':
-            text.remove(word)
-            continue
-        elif len(word) >= 4:
-            if word[0:4] == 'http':
-                text.remove(word)
-
-    
-            
-trainParser(smallDirectory);
+    # Calculate accuracy for 128 mnist test images
+    test_len = 128
+    test_data = mnist.test.images[:test_len].reshape((-1, n_steps, n_input))
+    test_label = mnist.test.labels[:test_len]
+    print("Testing Accuracy:", \
+        sess.run(accuracy, feed_dict={x: test_data, y: test_label}))
